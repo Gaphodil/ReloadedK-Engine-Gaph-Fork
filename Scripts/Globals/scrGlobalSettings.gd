3fa826da8274776f322107833e9ebe00cfb6a6b2
gaphodil <gaphodillo@gmail.com>
2024-04-08 10:56:53 -0700

    migrate section name to setting class to simplify dict
    fix bug with inc/dec value
    tweak getters
    move hud_scaling side effect to side effect function

--- Scripts/Globals/scrGlobalSettings.gd
+++ Scripts/Globals/scrGlobalSettings.gd
@@ -9,6 +9,7 @@ class Setting:
 	var default: Variant
 	var value: Variant:
 		set = set_value
+	var section: String
 	# Below values are ignored if type is bool.
 	var min_val: Variant
 	var max_val: Variant
@@ -19,7 +20,8 @@ class Setting:
 		_default: Variant,
 		_min_val: Variant = 0.0,
 		_max_val: Variant = 1.0,
-		_step: Variant = 0.5
+		_step: Variant = 0.5,
+		_section: String = "settings"
 	):
 		type = _type
 		default = _default
@@ -32,6 +34,7 @@ class Setting:
 			)
 		step = _step
 		value = _default
+		section = _section
 	
 	## Sets the value. Errors if the type does not match.
 	## If the type is int or float, it also clamps it.
@@ -52,36 +55,34 @@ class Setting:
 	func inc_value():
 		if type == TYPE_INT or type == TYPE_FLOAT:
 			value += step
-		assert(false, "Unimplemented for type" + type_string(type))
+			return
+		assert(false, "Unimplemented for type " + type_string(type))
 
 	## Decrements the value. Errors if not int or float.
 	func dec_value():
 		if type == TYPE_INT or type == TYPE_FLOAT:
 			value -= step
-		assert(false, "Unimplemented for type" + type_string(type))
+			return
+		assert(false, "Unimplemented for type " + type_string(type))
 
-## The dictionary of settings, structured similarly to
-## the config file. Keys are section names, values are
+## The dictionary of settings. Keys are section names, values are
 ## sub-dictionaries of setting names, and their type and value.
-## The setting value is duplicated to represent both default and actual values.
+## The valid range and config-file section is also specified in the object.
 var dict: Dictionary = {
-	"volume": {
-		"music_volume": Setting.new(TYPE_FLOAT, 1.0, 0.0, 1.0, 0.1),
-		"sound_volume": Setting.new(TYPE_FLOAT, 1.0, 0.0, 1.0, 0.1),
-	},
-	"settings": {
-		"fullscreen": Setting.new(TYPE_BOOL, false),
-		"zoom_scaling": Setting.new(TYPE_FLOAT, 1.0, 1.0, 2.0, 1.0),
-		"hud_scaling": Setting.new(TYPE_FLOAT, 1.0, 1.0, 1.5, 0.5),
-		"window_scaling": Setting.new(TYPE_FLOAT, 1.0, 1.0, 2.0, 0.5),
-		"vsync": Setting.new(TYPE_BOOL, true),
-		"autoreset": Setting.new(TYPE_BOOL, false),
-		"fps_display": Setting.new(TYPE_INT, FpsDisplay.OFF, 0, 2, 1),
-		"titlebar_stats": Setting.new(TYPE_INT, TitlebarStats.ALL, 0, 3, 1),
-		"extra_keys": Setting.new(TYPE_BOOL, false),
-		# Add more settings here! Format below, last three for int or float
-		# "name": Setting.new(type, default, [minimum, maximum, step])
-	},
+	"music_volume": Setting.new(TYPE_FLOAT, 1.0, 0.0, 1.0, 0.1, "volume"),
+	"sound_volume": Setting.new(TYPE_FLOAT, 1.0, 0.0, 1.0, 0.1, "volume"),
+	"fullscreen": Setting.new(TYPE_BOOL, false),
+	"zoom_scaling": Setting.new(TYPE_FLOAT, 1.0, 1.0, 2.0, 1.0),
+	"hud_scaling": Setting.new(TYPE_FLOAT, 1.0, 1.0, 1.5, 0.5),
+	"window_scaling": Setting.new(TYPE_FLOAT, 1.0, 1.0, 2.0, 0.5),
+	"vsync": Setting.new(TYPE_BOOL, true),
+	"autoreset": Setting.new(TYPE_BOOL, false),
+	"fps_display": Setting.new(TYPE_INT, FpsDisplay.OFF, 0, 2, 1),
+	"titlebar_stats": Setting.new(TYPE_INT, TitlebarStats.ALL, 0, 3, 1),
+	"extra_keys": Setting.new(TYPE_BOOL, false),
+	# Add more settings here! Format below, last three for int or float
+	# Can also add "section: name" if needed
+	# "name": Setting.new(type, default, [minimum, maximum, step])
 }
 
 ## Formatted strings for each setting.
@@ -133,45 +134,67 @@ func _ready():
 		load_settings()
 
 func get_setting(setting: String):
-	# Note that this won't function if a setting name is shared between sections
-	for section in dict.keys():
-		if dict[section].has(setting):
-			return dict[section][setting].value
+	var val = dict.get(setting)
+	if val:
+		return val.value
+	print_debug("Setting " + setting + " not found")
+
+func get_setting_type(setting: String):
+	var val = dict.get(setting)
+	if val:
+		return val.type
 	print_debug("Setting " + setting + " not found")
 
 ## Sets a value. Throws an error if the value is of the wrong type.
 func set_setting(setting: String, value):
-	for section in dict.keys():
-		if dict[section].has(setting):
-			assert(
-				typeof(value) == dict[section][setting].type,
-				"Setting " + setting + " must be of type " + type_string(dict[section][setting].type)
-			)
-			dict[section][setting].set_value(value)
-			apply_side_effects(setting)
-			return
+	if dict.has(setting):
+		dict[setting].set_value(value)
+		apply_side_effects(setting)
+		return
+	print_debug("Setting " + setting + " not found")
+
+## Increments a value. Throws an error if the value cannot be incremented.
+func inc_setting(setting: String):
+	if dict.has(setting):
+		dict[setting].inc_value()
+		apply_side_effects(setting)
+		return
+	print_debug("Setting " + setting + " not found")
+
+## Derements a value. Throws an error if the value cannot be decremented.
+func dec_setting(setting: String):
+	if dict.has(setting):
+		dict[setting].dec_value()
+		apply_side_effects(setting)
+		return
 	print_debug("Setting " + setting + " not found")
 
 ## Inverts a boolean setting. Throws an error if the setting is not a boolean.
 func flip_setting(setting: String):
-	for section in dict.keys():
-		if dict[section].has(setting):
-			assert(
-				typeof(dict[section][setting].value) == TYPE_BOOL,
-				"Setting " + setting + " must be of type bool"
-			)
-			dict[section][setting].set_value(not dict[section][setting].value)
-			apply_side_effects(setting)
-			return
+	if dict.has(setting):
+		assert(
+			typeof(dict[setting].value) == TYPE_BOOL,
+			"Setting " + setting + " must be of type bool"
+		)
+		dict[setting].set_value(not dict[setting].value)
+		apply_side_effects(setting)
+		return
 	print_debug("Setting " + setting + " not found")
 
-# Saves settings
+## Resets a specific setting to default.
+func reset_setting(setting: String):
+	if dict.has(setting):
+		dict[setting].reset_value()
+		apply_side_effects(setting)
+		return
+	print_debug("Setting " + setting + " not found")
+
+## Saves settings to the settings file.
 func save_settings() -> void:
 	var configFile := ConfigFile.new()
 
-	for section in dict.keys():
-		for setting in dict[section].keys():
-			configFile.set_value(section, setting, dict[section][setting].value)
+	for setting in dict.keys():
+		configFile.set_value(dict[setting].section, setting, dict[setting].value)
 	
 	for action in InputMap.get_actions():
 		configFile.set_value("controls", action, InputMap.action_get_events(action))
@@ -179,37 +202,41 @@ func save_settings() -> void:
 	configFile.save(DATA_PATH)
 
 ## Applies side effects, such as updating window or audio.
-## If the setting is "all", every side effect is reapplied.
+## If the setting is "all", every side effect is reapplied,
+## except for HUD scaling.
 func apply_side_effects(setting: String):
 	if setting == "music_volume" or setting == "all":
 		var music_bus: int = AudioServer.get_bus_index("Music")
 		AudioServer.set_bus_volume_db(
 			music_bus,
-			linear_to_db(dict["volume"]["music_volume"].value)
+			linear_to_db(dict["music_volume"].value)
 		)
 	if setting == "sound_volume" or setting == "all":
 		var sounds_bus: int = AudioServer.get_bus_index("Sounds")
 		AudioServer.set_bus_volume_db(
 			sounds_bus,
-			linear_to_db(dict["volume"]["sound_volume"].value)
+			linear_to_db(dict["sound_volume"].value)
 		)
 	if setting == "fullscreen" or setting == "window_scaling" or setting == "all":
 		GLOBAL_SETTINGS.set_window_mode()
 	if setting == "vsync" or setting == "all":
 		GLOBAL_SETTINGS.set_vsync_mode()
+	if setting == "hud_scaling":
+		# Sets HUD scaling by calling objHUDs method once
+		if is_instance_valid(objHUD):
+			objHUD.set_HUD_scaling()
 
 ## Load and set settings.
 func load_settings() -> void:
 	var configFile: = ConfigFile.new()
 	configFile.load(DATA_PATH)
 	
-	for section in dict.keys():
-		for setting in dict[section].keys():
-			dict[section][setting].set_value(
-				configFile.get_value(
-					section, setting, dict[section][setting].value
-				)
+	for setting in dict.keys():
+		dict[setting].set_value(
+			configFile.get_value(
+				dict[setting].section, setting, dict[setting].value
 			)
+		)
 	
 	apply_side_effects("all")
 	
@@ -219,18 +246,13 @@ func load_settings() -> void:
 		for event in configFile.get_value("controls", action):
 			InputMap.action_add_event(action, event)
 
-
 ## Resets and sets settings to their default values.
 func default_settings() -> void:
-	for section in dict.keys():
-		for setting in dict[section].keys():
-			dict[section][setting].reset_value()
-
+	for setting in dict.keys():
+		dict[setting].reset_value()
 	apply_side_effects("all")
+	apply_side_effects("hud_scaling")
 
-	# Sets HUD scaling by calling objHUDs method once
-	if is_instance_valid(objHUD):
-		objHUD.set_HUD_scaling()
 
 ## Sets the game's window mode by checking the fullscreen setting.
 func set_window_mode():
@@ -240,7 +262,6 @@ func set_window_mode():
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		# DisplayServer.window_set_size(Vector2(INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT))
 		set_window_scale()
-
 
 ## Sets the game's vsync mode by checking the vsync setting.
 func set_vsync_mode():
