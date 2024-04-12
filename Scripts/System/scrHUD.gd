6630bdca3566d556d421bdecfa862819260155fd
gaphodil <gaphodillo@gmail.com>
2024-04-11 22:49:27 -0700

    add dev option for calculated fps

--- Scripts/System/scrHUD.gd
+++ Scripts/System/scrHUD.gd
@@ -6,15 +6,41 @@ var item_text: String = ""
 @onready var container_timer: Node = $Display/MarginContainer2/Timer
 @onready var fps_container: Node = $Display/MarginContainer3
 var fps_fadeout_amt: float = -1.0
+var manual_frame_counter: Array = []
+var last_four_fps: Array[int] = [120, 120, 120, 120]
+var manual_fps: int = 480
+
+## If this is true, FPS will be calculated every frame
+## instead of using the Engine value every second.
+var avg_fps: bool = false
 
 func _ready():
 	$Display/MarginContainer2.set_visible(false)
 	fps_container.set_visible(false)
 	set_HUD_scaling()
 	handle_debug_mode()
+	# Initiate manual frame counter with 120 copies of current time
+	for _i in range(120):
+		manual_frame_counter.append(Time.get_ticks_msec())
 	handle_fps_indic(0)
 
+func _process(_delta):
+	if avg_fps:
+		# Add current time
+		manual_frame_counter.append(Time.get_ticks_msec())
+		# Remove any that are older than 0.25 seconds
+		while manual_frame_counter.size() > 0 and manual_frame_counter[0] < Time.get_ticks_msec() - 250:
+			manual_frame_counter.pop_front()
+
 func _physics_process(delta):
+	if avg_fps:
+		# Pop the last four fps and replace it with the current sum
+		last_four_fps.pop_front()
+		last_four_fps.append(manual_frame_counter.size())
+		manual_fps = 0
+		for val in last_four_fps:
+			manual_fps += val
+
 	handle_debug_mode()
 	handle_fps_indic(delta)
 
@@ -38,7 +64,8 @@ func handle_debug_mode() -> void:
 			$Display/MarginContainer.set_visible(true)
 			$Display/MarginContainer/VBoxContainer/textDebug1.text = str(" Player X: ", round(GLOBAL_INSTANCES.objPlayerID.position.x), " ")
 			$Display/MarginContainer/VBoxContainer/textDebug2.text = str(" Player Y: ", round(GLOBAL_INSTANCES.objPlayerID.position.y), " ")
-			$Display/MarginContainer/VBoxContainer/textDebug3.text = str(" FPS: %d" % Engine.get_frames_per_second(), " ")
+			var fps: int = manual_fps if avg_fps else int(Engine.get_frames_per_second())
+			$Display/MarginContainer/VBoxContainer/textDebug3.text = str(" FPS: %d" % fps, " ")
 			
 			# Extra check added for v4.2 compatibility
 			if get_tree().get_current_scene() != null:
@@ -75,7 +102,7 @@ func handle_fps_indic(delta):
 		GLOBAL_SETTINGS.get_setting("fps_display")
 	)
 	if not fps_setting == GLOBAL_SETTINGS.FpsDisplay.OFF:
-		var fps = Engine.get_frames_per_second()
+		var fps: int = manual_fps if avg_fps else int(Engine.get_frames_per_second())
 		var label = fps_container.get_child(0)
 		label.text = str(fps)
 		if (
